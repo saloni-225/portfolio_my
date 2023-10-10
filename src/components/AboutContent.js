f7597740601e6cad3d38affc34fc3bb38412b7bf
Saloni Kumari <salonikri225@gmail.com>
2023-10-10 13:26:21 +0530

    my first commit

---
+++ src/components/AboutContent.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import "./AboutContentStyles.css";
+import React from 'react'
+import react1 from "../assets/project1.png"
+import react2 from "../assets/project2.png"
+
+const AboutContent = () => {
+    return (
+        <div className="about">
+            <div className="left">
+                <h1>Who Am I?</h1>
+                <p>I am a React Front-End Developer having a strong foundation in DBMS and a keen interest in AIML</p>
+                <Link to={"/contact"}>
+                    <button className="btn">Contact</button>
+                </Link>
+            </div>
+            <div className="right">
+                <div className="img-container">
+                    <div className="img-stack top">
+                        <img src={react1} className="img" alt="true">
+                        </img>
+                    </div>
+                    <div className="img-stack bottom">
+                        <img src={react2} className="img" alt="true">
+                        </img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutContent
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,17 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Portfolio",
+        text: "The project involves building a portfolio using React, a JavaScript library. It aims to showcase skills, projects, and achievements in an interactive and visually appealing manner, enhancing personal branding and professional presentation.",
+        view: "https://github.com/saloni-225/Portfolio"
+    },
+    {
+        imgsrc: pro2,
+        title: "Bike Price Detector",
+        text: "Basically this is a group project in which I have contributed to Front-End Development and Machine Learning.This project is an innovative application that utilizes machine learning algorithms to forecast the prices of bikes. By analyzing various factors such as brand, model, year, mileage, and condition, the project provides accurate price predictions.",
+        view: "https://github.com/saloni-225/Bike-Price-Prediction"
+    },
+]
+export default ProjectCardData
